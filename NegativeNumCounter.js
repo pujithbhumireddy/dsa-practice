2506fc48c309d993213eb79bd8108b4007bf5ee4
pujithbhumireddy <pujithbhumireddy@gmail.com>
2025-08-13 10:51:41 +0530

    NegativeNumCounter.js

---
+++ NegativeNumCounter.js
@@ -0,0 +1,18 @@
+// write a function that returns the number of negative numbers in an array
+
+function NegativeNumCounter(arr) {
+  let count = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] < 0) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+let arr = [5, 6, -1, -4, -6, -5, -5, 7, 8, 9, 0];
+
+let solution = NegativeNumCounter(arr);
+
+console.log(solution);
