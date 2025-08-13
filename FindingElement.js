54be47a48939511c389bf973c1f22ce751b686d4
pujithbhumireddy <pujithbhumireddy@gmail.com>
2025-08-13 09:50:10 +0530

    findelement

---
+++ FindingElement.js
@@ -0,0 +1,11 @@
+//write a function that searches for an element in an array and returns the index, if the element is not present just return -1
+
+let arr = [3, 4, 5, 61, 7, 8];
+
+for (let index = 0; index < arr.length; index++) {
+  // const element = array[index];
+
+  if (arr[index] == 5) {
+    console.log(index);
+  }
+}
