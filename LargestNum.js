fd0b98b4c293cfebf39fa65daf105ac91b33dcbf
pujithbhumireddy <pujithbhumireddy@gmail.com>
2025-08-13 11:41:21 +0530

    LargestNum.js modified

--- LargestNum.js
+++ LargestNum.js
@@ -1,6 +1,7 @@
 // write a function that return the largest number in an array
 
 function LargestNumInArr(arr) {
+  //   let max = -Infinity;
   let max = arr[0];
 
   for (let i = 0; i < arr.length; i++) {
@@ -13,5 +14,6 @@ function LargestNumInArr(arr) {
 }
 
 let arr = [5, 66, 77, 88, 100, 45, 67, 23];
+// let arr = [-6, -4, -7, -12, -11];
 let sol = LargestNumInArr(arr);
 console.log(sol);
