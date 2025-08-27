67e4560f6590248a25f8fab9b2857558801fa646
pujithbhumireddy <pujithbhumireddy@gmail.com>
2025-08-27 18:10:45 +0530

    added patternThree.js

--- Patterns/patternTwo.js
+++ Patterns/patternTwo.js
@@ -6,6 +6,24 @@ for (let i = 0; i < n; i++) {
   for (let j = 0; j <= i; j++) {
     row = row + "*";
   }
+  console.log(row);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let row = " ";
+
+  for (let j = 1; j <= i; j++) {
+    row = row + j + " ";
+  }
+
+  console.log(row);
+}
+
+for (let i = 1; i <= 5; i++) {
+  let row = " ";
+  for (let j = 1; j <= i; j++) {
+    row = row + i + " ";
+  }
 
   console.log(row);
 }
